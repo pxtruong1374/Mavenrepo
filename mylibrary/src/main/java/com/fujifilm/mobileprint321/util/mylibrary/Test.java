fea040288eac1e8aa97d57ca5cfa34f42d8cffab
Truong. Pham Xuan [Programer] <pxtruong@isb-vietnam.com>
2019-07-01 16:10:48 +0700

    init commit

---
+++ mylibrary/src/main/java/com/fujifilm/mobileprint321/util/mylibrary/Test.java
@@ -0,0 +1,10 @@
+package com.fujifilm.mobileprint321.util.mylibrary;
+
+import android.util.Log;
+
+public class Test {
+    public static void testMaven()
+    {
+        Log.e("TestMaven", "test");
+    }
+}
